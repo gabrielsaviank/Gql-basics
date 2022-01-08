40ea166b2c4c44868f5f4342737a60a7a55bb4f3
gabrielsavian <gabrielsvnzrd@gmail.com>
2022-01-08 10:22:46 -0300

    feat: added json server

--- schema/schema.ts
+++ schema/schema.ts
@@ -14,25 +14,25 @@ const users = [
 ]
 
 const UserType = new GraphQLObjectType({
-   name: "User",
-   fields: {
-       id: { type: GraphQLString},
-       firstName: { type: GraphQLString },
-       age: { type: GraphQLInt }
-   }
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString},
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+    }
 });
 
 const RootQuery = new GraphQLObjectType({
-   name: "RootQueryType",
-   fields: {
-       user: {
-           type: UserType,
-           args: { id: { type: GraphQLString } },
-           resolve( parentValue, args) {
-               return _.find (users, { id: args.id });
-           }
-       }
-   }
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLObjectType } },
+            resolve(parentValue, args){
+                return _.find(users, { id: args.id })
+            }
+        }
+    }
 });
 
 module.exports = new GraphQLSchema({
